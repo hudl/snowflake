8dbed711fd62406e8f00918cbfd8693a132fa4a7
Matt Munger <matt.munger@hudl.com>
2018-07-17 16:13:48 -0500

    trying to pull in uniform

---
+++ components/Uniform.js
@@ -0,0 +1,25 @@
+const {
+  AvatarUser,
+  Button,
+  Icon,
+  Link,
+  LookupSelect,
+  Headline,
+  Subhead,
+  Text,
+  ItemTitle,
+  FormModifier
+} = window.__uniformui_Components;
+
+export {
+  AvatarUser,
+  Button,
+  Icon,
+  Link,
+  LookupSelect,
+  Headline,
+  Subhead,
+  Text,
+  ItemTitle,
+  FormModifier
+};
---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link
+            href="https://sc-ui.hudl.com/uniform-ui-css/1/uniform-ui.css.gz"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <script src="https://s3.amazonaws.com/hudl-ui/uniform-ui-components/all/3/uniform-ui-components_vendor.js.gz" />
+          <script src="https://s3.amazonaws.com/hudl-ui/uniform-ui-components/all/3/uniform-ui-components.js.gz" />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
