ba74f27dd9595fe33e475012e9fd14897fbea80b
Matt Munger <matt.munger@hudl.com>
2018-11-29 13:43:52 -0600

    add link to Hudlies title search

--- components/TitleSelector.js
+++ components/TitleSelector.js
@@ -5,8 +5,14 @@ import { hudlRoles } from '../constants';
 import type { MilestoneMap } from '../constants';
 
 let LookupSelect = undefined;
+let Icon = undefined;
+let Tooltip = undefined;
+let Link = undefined;
 if (typeof window !== 'undefined') {
   LookupSelect = window.__uniformui_Components.LookupSelect;
+  Icon = window.__uniformui_Components.Icon;
+  Tooltip = window.__uniformui_Components.Tooltip;
+  Link = window.__uniformui_Components.Link;
 }
 
 type Props = {
@@ -21,14 +27,45 @@ class TitleSelector extends React.Component<Props> {
       this.props.currentTitle !== '' ? this.props.currentTitle : null;
     if (typeof window !== 'undefined') {
       return (
-        <LookupSelect
-          placeholder="Find a role"
-          options={hudlRoles}
-          id="role-select"
-          onChange={this.props.setTitleFn}
-          value={currentValue}
-          showAllOptions
-        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center'
+          }}
+        >
+          <div style={{ flexGrow: 1 }}>
+            <LookupSelect
+              placeholder="Find a role"
+              options={hudlRoles}
+              id="role-select"
+              onChange={this.props.setTitleFn}
+              value={currentValue}
+              showAllOptions
+            />
+          </div>
+          {currentValue !== null && (
+            <Tooltip
+              position="bottom"
+              text="Find Hudlies with this title"
+              parent="icon"
+            >
+              <Link
+                href={`https://hudlies.hudltools.com/?search=title:${encodeURI(
+                  currentValue
+                )}`}
+                target="_blank"
+                type="wrapper"
+              >
+                <Icon
+                  type="share"
+                  color="nonessential"
+                  className="uni-margin--half--left"
+                />
+              </Link>
+            </Tooltip>
+          )}
+        </div>
       );
     }
     return <div />;
